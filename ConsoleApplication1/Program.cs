aa4a4d5fd0fc8a23cfe968eb6d42931e69f7cb4b
reggiec <reggiec@hotmail.com>
2019-07-26 21:42:14 -0400

    SRP Example

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using SRP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OCP();
+        }
+
+
+        static void OCP()
+        {
+            var report = new WorkReport();
+            report.AddEntry(new WorkReportEntry { ProjectCode = "123Ds", ProjectName = "Project1", SpentHours = 5 });
+            report.AddEntry(new WorkReportEntry { ProjectCode = "987Fc", ProjectName = "Project2", SpentHours = 3 });
+
+            Console.WriteLine(report.ToString());
+
+            var saver = new FileSaver();
+            saver.SaveToFile(@"C:/temp/Reports", "WorkReport.txt", report);
+        }
+
+    }
+}
---
+++ SRP/FileSaver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+public class FileSaver
+{
+    public void SaveToFile<T>(string directoryPath, string fileName, IEntryManager<T> workReport)
+    {
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+ 
+            File.WriteAllText(Path.Combine(directoryPath, fileName), workReport.ToString());
+        }
+    }
+}
+

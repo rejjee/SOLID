c312659ed050d7ac63408436489308f5e75b7048
reggiec <reggiec@hotmail.com>
2019-07-27 19:07:46 -0400

    pre-OCP Computer Monitor example

---
+++ OCP/ComputerMonitor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    public enum MonitorType
+    {
+        OLED,
+        LCD,
+        LED
+    }
+
+    public enum Screen
+    {
+        WideScreen,
+        CurvedScreen
+    }
+
+    public class ComputerMonitor
+    {
+        public string Name { get; set; }
+        public MonitorType Type { get; set; }
+        public Screen Screen { get; set; }
+    }
+
+    public class MonitorFilter
+    {
+        public List<ComputerMonitor> FilterByType(IEnumerable<ComputerMonitor> monitors, MonitorType type)
+        {
+            return monitors.Where(m => m.Type == type).ToList();
+        }
+
+        public List<ComputerMonitor> FilterByType(IEnumerable<ComputerMonitor> monitors, Screen screen)
+        {
+            return monitors.Where(m => m.Screen == screen).ToList();
+        }
+    }
+
+}
--- OCP/Program.cs
+++ OCP/Program.cs
@@ -10,6 +10,12 @@ namespace OCP
     {
         static void Main(string[] args)
         {
+            //CalculateTotalSalaries();
+            FilterByType();
+        }
+
+        static void CalculateTotalSalaries()
+        {
             var devReports = new List<DeveloperReport>
             {
                 new SrDeveloper {Id = 1, Name = "Dev1", Level = "Senior developer", HourlyRate  = 30.5, WorkingHours = 160 },
@@ -19,6 +25,28 @@ namespace OCP
 
             var calculator = new SalaryCalculator(devReports);
             Console.WriteLine(string.Format("Sum of all the developer salaries is {0} dollars", calculator.CalculateTotalSalaries()));
+        }
+
+        static void FilterByType()
+        {
+            var monitors = new List<ComputerMonitor>
+            {
+                new ComputerMonitor { Name = "Samsung S345", Screen = Screen.CurvedScreen, Type = MonitorType.OLED },
+                new ComputerMonitor { Name = "Philips P532", Screen = Screen.WideScreen, Type = MonitorType.LCD },
+                new ComputerMonitor { Name = "LG L888", Screen = Screen.WideScreen, Type = MonitorType.LED },
+                new ComputerMonitor { Name = "Samsung S999", Screen = Screen.WideScreen, Type = MonitorType.OLED },
+                new ComputerMonitor { Name = "Dell D2J47", Screen = Screen.CurvedScreen, Type = MonitorType.LCD }        
+            };
+
+            var filter = new MonitorFilter();
+
+            var lcdMonitors = filter.FilterByType(monitors, MonitorType.LCD);
+            Console.WriteLine("All LCD monitors");
+            foreach (var monitor in lcdMonitors)
+            {
+                string string1 = string.Format("Name: {0}, Type: {1}, Screen: {2}", monitor.Name, monitor.Type, monitor.Screen);
+                Console.WriteLine(string1);
+            }
         }
     }
 }
